3e6051db633155a89ebcbe03182347f2673ead81
TheFireUrinal <kyle.c.litwack@gmail.com>
2025-07-31 14:37:45 -0800

    Create Example 1.ps1

---
+++ Scripts/Example 1.ps1
@@ -0,0 +1,21 @@
+# Set computer name script by FireUrinal
+
+#gets asset tag number
+$devicefas = (Get-WmiObject -Class Win32_SystemEnclosure).SMBiosAssetTag
+
+#sets the remainder of the device name
+$classname = "Maint-"
+
+#combines $classname and $devicefas
+$finalname = $classname + $devicefas
+
+#this sets the device name during a task sequence and does not work unless in a task sequence
+try {
+  $tsenv = New-Object -ComObject Microsoft.SMS.TSEnvironment
+  $tsenv.Value("OSDComputerName") = $finalname
+  Write-Output 0
+}
+#this is just for testing the script to make sure the naming function works and there are no typos
+catch {
+  Write-Output $finalname
+}
